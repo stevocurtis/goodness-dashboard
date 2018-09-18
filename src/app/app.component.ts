9ba3325273e52ca050e3147614c52ed43d7d8cdc
stevocurtis <myste123>
2018-09-18 22:30:55 +0100

    Started to play with templates and change events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'goodness-dashboard';
+  // title = "goodness-dashboard";
+  selectedValue: string = undefined;
+
+  // event handler for the select element's change event
+  selectChangeHandler(event: any) {
+    // update the ui
+    this.selectedValue = event.target.value;
+  }
 }
