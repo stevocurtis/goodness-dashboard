ea51be2ea8af32e733e0df7c77ac41558c5093ae
stevocurtis <myste123>
2019-08-19 21:20:54 +0100

    Added some pages to demo routing

---
+++ gd-vue-ui/src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact", // this is the name of the component
+  data() {
+    return {
+      email: "stevo.curtis@gmail.com",
+      web: "http://www.vitezeo.com"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
